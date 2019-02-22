a493eb3360fdb7934b004782e2ee3f19b833a73f
timothycosta <timothycosta@gmail.com>
2019-02-22 10:43:01 +0900

    Use 'setter' selector syntax

--- TSCThemer/Classes/UIView+Themer.swift
+++ TSCThemer/Classes/UIView+Themer.swift
@@ -9,43 +9,43 @@ import Foundation
 import UIKit
 
 extension NSObject {
-	public func setThemes<T>(with objects: [T], selector:String, themer: Themer = Themer.shared) {
-		assert(self.responds(to: Selector(selector)), "Invalid selector: '\(selector)'")
-		let sel = Selector(selector)
+	public func setThemes<T>(with objects: [T], selector:Selector, themer: Themer = Themer.shared) {
+		assert(self.responds(to: selector), "Invalid selector: '\(selector)'")
 		for (idx, val) in objects.enumerated() {
-			self.setTheme(themer: themer, index: idx, key: selector, block: { (obj) in
-				let _ = obj.perform(sel, with: val)
+			self.setTheme(themer: themer, index: idx, key: NSStringFromSelector(selector), block: { (obj) in
+				let _ = obj.perform(selector, with: val)
 			})
 		}
 	}
+
 }
 
 extension UIView {
 	public func themeBackgroundColor(_ colors: [UIColor]) {
-		self.setThemes(with: colors, selector: "setBackgroundColor:")
+		self.setThemes(with: colors, selector: #selector(setter: UIView.backgroundColor))
 	}
 	public func themeTintColor(_ colors: [UIColor]) {
-		self.setThemes(with: colors, selector: "setTintColor:")
+		self.setThemes(with: colors, selector: #selector(setter: UIView.tintColor))
 	}
 }
 
 extension UILabel {
 	public func themeText(_ texts: [String]) {
-		self.setThemes(with: texts, selector: "setText:")
+		self.setThemes(with: texts, selector: #selector(setter: UILabel.text))
 	}
 	public func themeAttributedText(_ texts: [NSAttributedString]) {
-		self.setThemes(with: texts, selector: "setAttributedText:")
+		self.setThemes(with: texts, selector: #selector(setter: UILabel.attributedText))
 	}
 	public func themeTextColors(_ colors: [UIColor]) {
-		self.setThemes(with: colors, selector: "setTextColor:")
+		self.setThemes(with: colors, selector: #selector(setter: UILabel.textColor))
 	}
 }
 
 extension CALayer {
 	public func themeBackgroundColor(_ colors: [UIColor]) {
-		self.setThemes(with: colors.map( { $0.cgColor } ), selector: "setBackgroundColor:")
+		self.setThemes(with: colors.map( { $0.cgColor } ), selector: #selector(setter: CALayer.backgroundColor))
 	}
 	public func themeBorderColor(_ colors: [UIColor]) {
-		self.setThemes(with: colors.map( { $0.cgColor } ), selector: "setBorderColor:")
+		self.setThemes(with: colors.map( { $0.cgColor } ), selector: #selector(setter: CALayer.borderColor))
 	}
 }
